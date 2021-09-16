ee4c32a201647d54692c2795b1cf2d2895ae7457
KyawWinTun-vix <vixenhello1998@gmail.com>
2021-09-16 08:31:39 +0630

    age

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+const video = document.getElementById('video');
+let predictedAges = [];
 
 Promise.all([
       faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
       faceapi.nets.faceExpressionNet.loadFromUri('/models'),
       faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
       faceapi.nets.ageGenderNet.loadFromUri('/models')
-]).then(initVideo());
+]).then(initVideo);
 
 function initVideo(){ 
       navigator.mediaDevices.getUserMedia({
@@ -29,24 +32,45 @@ function initVideo(){
 
       // display canvas size
       const displaySize = {width: video.width, height: video.height};
+      faceapi.matchDimensions(canvas, displaySize);
 
       setInterval(async function(){
 
-            const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender();
-            console.log(detections);
+            const detections = await faceapi
+            .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceLandmarks()
+            .withFaceExpressions()
+            .withAgeAndGender();
+            // console.log(detections);
 
             const resizedDetections = faceapi.resizeResults(detections, displaySize) //resize display
             canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height); //clear old canvas
             faceapi.draw.drawDetections(canvas, resizedDetections)
             faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-            // faceapi.draw.drawAgeAndGender(canvas, resizedDetections)
-            
-            const minProbability = 0.05
-            faceapi.draw.drawFaceExpressions(canvas, resizedDetections, minProbability)
-            faceapi.draw.drawAgeAndGender(canvas, resizedDetections)
+            faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+            // console.log(resizedDetections);
+
+            const age = resizedDetections[0].age;
+            const interpolatedAge = interpolatedAgePredictions(age);
+            const bottomRight = {
+                  x : resizedDetections[0].detection.box.bottomRight.x - 50,
+                  y : resizedDetections[0].detection.box.bottomRight.y
+            }
+
+            new faceapi.draw.DrawTextField([
+                  
+                  `${faceapi.utils.round(interpolatedAge, 0)} years`
+            ], bottomRight).draw(canvas);
 
             
 
       }, 100);
  });
+
+ function interpolatedAgePredictions(age){
+       predictedAges = [age].concat(predictedAges).slice(0, 30);
+       const avgPredictedAge = 
+            predictedAges.reduce((total, a) => total + a) / predictedAges.length;
+            return avgPredictedAge;
+ }
 
